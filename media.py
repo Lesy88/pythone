154a31210e05811cff301cf36d151e3159da03a5
Lesya <lisenokgalaxy@gmail.com>
2025-01-27 10:26:25 +0100

    Add files via upload

--- media.py
+++ media.py
@@ -5,7 +5,7 @@ while (risposta != 'fine') :
     numero_float = float(input ("metti numero:"))
     somma_numeri += numero_float
     contatore += 1 
-    risposta = input ('fine?')
+    risposta = input ('fine?  \n')
 print ('La tua media è ', somma_numeri/contatore)
 
 
