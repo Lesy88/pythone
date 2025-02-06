db7845ea8c17bc366dc0fc6a423719dfd2c8b53f
Lesya <lisenokgalaxy@gmail.com>
2025-02-06 09:36:46 +0100

    Add files via upload

--- esempio_01.py
+++ esempio_01.py
@@ -2,19 +2,16 @@ i = 0
 countZero = 0
 somma = 0
 while True :
-    stringa = input ("Inserisci il numero:")
-    numero = float (stringa)
+    numero = float (input())
     if numero == 0 :
         countZero += 1
-        stringa = input ("Inserisci il numero:")
-        numero = float (stringa)
+        numero = float (input())
         if numero == 0 :
             countZero += 1
         if countZero == 2:
             break
         else:
             countZero = 0
-            print ("countZero è:", countZero)
 
     somma += numero
 
