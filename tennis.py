8cd3b934f0a7a186f5c37f2975b68e18ebb6f7fe
Lesya <lisenokgalaxy@gmail.com>
2025-02-10 21:50:40 +0100

    Add files via upload

--- tennis.py
+++ tennis.py
@@ -2,14 +2,15 @@ fineGioco = False
 puntiGiocatore1 = 0
 puntiGiocatore2 = 0
 while not fineGioco:
-    numeroGiocatore = int(input("1 or 2"))
-    print(numeroGiocatore)
+    numeroGiocatore = int(input("metti numero di giocatore: 1 oppure 2 "))
     if numeroGiocatore == 1:
         puntiGiocatore1 += 1
-        print(puntiGiocatore1,puntiGiocatore2)
     else:
         puntiGiocatore2 += 1
-        print(puntiGiocatore1,puntiGiocatore2)
-    if (puntiGiocatore2 == 4 or puntiGiocatore1 == 4):
+    if (puntiGiocatore1 == 4 and puntiGiocatore2 <= 2) or (puntiGiocatore1 >=6 and puntiGiocatore1-puntiGiocatore2>=2):
+        print("Ha vinto giocatore 1")
+        fineGioco = True
+    if (puntiGiocatore2 == 4 and puntiGiocatore1 <= 2) or (puntiGiocatore2 >=6 and puntiGiocatore2-puntiGiocatore1>=2):
+        print("Ha vinto giocatore 2")
         fineGioco = True
 print(puntiGiocatore1,puntiGiocatore2)
