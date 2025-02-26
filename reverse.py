43986b0a289bab38472fc12da670235fb7c4ff07
Lesya <lisenokgalaxy@gmail.com>
2025-02-26 13:29:51 +0100

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,22 @@
+CONTINUARE = True
+import os
+import sys
+# Creare una lista vuota
+lista = []
+nome=input("Inserire il nome del file TXT")
+if not os.path.exists(nome):
+    print ("Il file non esiste")
+    sys.exit()
+
+# Lettura del file di testo con la chiusura automatica
+with open(nome, mode="r") as fh:
+    while CONTINUARE :
+        for riga in fh:
+            if riga == "NO":
+                CONTINUARE = False
+            else:
+            # Invertire la riga e aggiungerla alla lista
+                riga_invertita = riga[::-1]
+                lista.append(riga_invertita)
+
+print("La lista invertita è:", lista)
